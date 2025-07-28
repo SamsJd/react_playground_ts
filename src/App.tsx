2da9335f79374ec8aba8f54731dc077971df205e
Sâmara Jeise Dias <samarajeise@hotmail.com>
2025-07-27 22:30:02 -0300

    Adicionando o Componente Login no Componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
-import Tarefa from './pages/tarefa/Tarefa';
+import './App.css'
+import Login from './pages/login/Login'
 
 function App() {
+
   return (
     <>
-      <Tarefa />
+      <Login />
     </>
   )
 }
